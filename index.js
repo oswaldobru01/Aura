eb6ac6887909f93532fb36fa4c93fcfcb439b616
Oswaldobru01 <oswaldobru01@gmail.com>
2024-08-09 17:32:23 -0500

    Cambios

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const port = 3000;
 const { Schema } = mongoose;
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 const cors = require('cors');
-// Middleware para parsear el cuerpo de las solicitudes
+
 app.use(bodyParser.json());
 app.use(cors());
-// Configuración de conexión a MongoDB
+
 const uri = "mongodb+srv://oswaldobru01:Ob010796@aura.atgra.mongodb.net/?retryWrites=true&w=majority&appName=Aura";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Definición de un esquema y modelo
 const ItemSchema = new Schema({
   idTipoArticulo: {
     type: Number,
@@ -49,22 +48,18 @@ const ItemSchema = new Schema({
     type: Number
   }
 }, {
-  versionKey: false // Esto elimina el campo __v
+  versionKey: false 
 });
-// Aplicar el plugin de auto-incremento en el campo idTipoArticulo
 ItemSchema.plugin(AutoIncrement, { inc_field: 'idTipoArticulo' });
 
-// Middleware para calcular el costoTotal antes de guardar
 ItemSchema.pre('save', function(next) {
   this.costoTotal = this.items.reduce((sum, item) => sum + item.costo, 0);
   next();
 });
 
-// Middleware para calcular el costoTotal después de actualizar un item
 ItemSchema.pre('findOneAndUpdate', async function(next) {
   const update = this.getUpdate();
   if (update.$set && update.$set['items.$.costo']) {
-    // Buscar el documento actualizado
     const docToUpdate = await this.model.findOne(this.getQuery());
     // Recalcular el costoTotal
     docToUpdate.costoTotal = docToUpdate.items.reduce((sum, item) => sum + item.costo, 0);
@@ -75,9 +70,6 @@ ItemSchema.pre('findOneAndUpdate', async function(next) {
 
 
 const Item = mongoose.model('Item', ItemSchema);
-// Middleware para calcular el costoTotal antes de guardar
-
-// Ruta para obtener todos los items
 app.get('/aura', async (req, res) => {
   try {
     const items = await Item.find();
@@ -86,18 +78,14 @@ app.get('/aura', async (req, res) => {
     res.status(500).json({ message: 'Error al obtener los items' });
   }
 });
-
-// Ruta para crear un nuevo item
 app.post('/aura', async (req, res) => {
   try {
-    // Verificar si ya existe un item con el mismo nombreArticulo
     const existingItemCount = await Item.countDocuments({ nombreArticulo: req.body.nombreArticulo });
 
     if (existingItemCount > 0) {
       return res.status(400).json({ message: `No se puede duplicar el item con nombreArticulo: ${req.body.nombreArticulo}` });
     }
 
-    // Si no se proporciona idTipoArticulo, el plugin lo calculará automáticamente
     const newItem = new Item(req.body);
     await newItem.save();
     res.status(201).json(newItem);
@@ -110,7 +98,6 @@ app.delete('/aura/eliminarArticulo/:tipoArticulo/:nombreArticulo', async (req, r
   try {
     const { tipoArticulo, nombreArticulo } = req.params;
 
-    // Intentar encontrar y eliminar el artículo que coincide con tipoArticulo y nombreArticulo
     const item = await Item.findOneAndDelete({ tipoArticulo, nombreArticulo });
 
     if (!item) {
@@ -119,7 +106,6 @@ app.delete('/aura/eliminarArticulo/:tipoArticulo/:nombreArticulo', async (req, r
 
     res.status(200).json({ message: 'Artículo eliminado', item });
   } catch (error) {
-    // Manejo de errores detallado
     res.status(500).json({ message: 'Error al eliminar el artículo', error: error.message });
   }
 });
@@ -128,25 +114,20 @@ app.delete('/aura/eliminarItem/:tipoArticulo/:nombreArticulo/:material', async (
   try {
     const { tipoArticulo, nombreArticulo, material } = req.params;
 
-    // Buscar el documento específico por tipoArticulo y nombreArticulo
     const item = await Item.findOne({ tipoArticulo, nombreArticulo });
 
     if (!item) {
       return res.status(404).json({ message: 'Artículo no encontrado' });
     }
 
-    // Filtrar los items para eliminar el que coincide con el material
     const updatedItems = item.items.filter(i => i.material !== material);
 
-    // Si el length no cambia, significa que el material no se encontró
     if (updatedItems.length === item.items.length) {
       return res.status(404).json({ message: 'Material no encontrado en el artículo' });
     }
 
-    // Actualizar el array de items
     item.items = updatedItems;
 
-    // Guardar los cambios
     await item.save();
 
     res.status(200).json({ message: 'Material eliminado', item });
@@ -156,14 +137,11 @@ app.delete('/aura/eliminarItem/:tipoArticulo/:nombreArticulo/:material', async (
 });
 
 
-
-// Ruta para actualizar un item específico por tipoArticulo y material
 app.put('/aura/:tipoArticulo/:nombreArticulo/:material', async (req, res) => {
   try {
     const { tipoArticulo, nombreArticulo, material } = req.params;
     const { nuevoMaterial, cantidad, unidad, costo } = req.body;
 
-    // Actualizar el item específico dentro del array items
     const item = await Item.findOneAndUpdate(
       { tipoArticulo, nombreArticulo, 'items.material': material },
       {
